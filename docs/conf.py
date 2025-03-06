f4346405341e04eb9d4239cd02b7a2729c9c6db5
Richard Jost <rjost@settlor.com>
2025-03-06 13:11:22 -0700

    Fix extras_require syntax

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ master_doc = "index"
 project = "Cloud Storage"
 copyright = "Copyright 2017-2021 Scott Werner"
 author = "Scott Werner"
-version = release = "0.11.0"
+version = release = "0.11.1"
 
 # on_rtd is whether we are on readthedocs.org
 on_rtd = os.environ.get("READTHEDOCS", None) == "True"
